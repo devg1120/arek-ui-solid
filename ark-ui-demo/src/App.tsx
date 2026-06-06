b81bd2141ef9d5dfe01e2221aec9df133bbb062b
devg1120 <devg1120@gmail.com>
2026-06-06 17:03:06 +0900

    up

--- ark-ui-demo/src/App.tsx
+++ ark-ui-demo/src/App.tsx
@@ -6,10 +6,21 @@ import { NestedSplitter } from './components/splitter'
 import { BasicTreeView } from './components/tree-view'
 import { BasicDatePicker } from './components/date-picker'
 
+import { NestedMenu } from './components/menu'
+import { ControlledPosition } from './components/floating-panel'
+
 function App() {
 
 return (
 <>
+  <br/>
+  <h4>ControlledPosition</h4>
+  <ControlledPosition />
+
+  <br/>
+  <h4>NestedMenu</h4>
+  <NestedMenu />
+
   <br/>
   <h4>BasicDatePicker</h4>
   <BasicDatePicker />
